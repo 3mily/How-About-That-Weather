c499778c5b8ddf0dd02b60932d6fa9f8c6c07919
3mily <emrcraig@gmail.com>
2015-02-12 21:01:38 +0000

    initial commit

---
+++ practice.js
@@ -0,0 +1,13 @@
+$(function(){
+
+
+  $("#square").on('click', function() {
+    $(this).addClass('slide');
+  });
+  $("#square").on('click', function() {
+    $(this).fadeOut(2000);
+  });
+
+
+
+});
